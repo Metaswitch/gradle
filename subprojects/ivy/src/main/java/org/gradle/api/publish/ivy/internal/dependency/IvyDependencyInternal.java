77422a3be3428263d47c129bbc4e73f1b569a135
Jackson Arlidge <jackson.arlidge@metaswitch.com>
2019-10-18 03:19:09 +0100

    Combine dependency lines that only differ by conf

--- subprojects/ivy/src/main/java/org/gradle/api/publish/ivy/internal/dependency/IvyDependencyInternal.java
+++ subprojects/ivy/src/main/java/org/gradle/api/publish/ivy/internal/dependency/IvyDependencyInternal.java
@@ -27,4 +27,6 @@ public interface IvyDependencyInternal extends IvyDependency {
     Iterable<ExcludeRule> getExcludeRules();
 
     ImmutableAttributes getAttributes();
+
+    void setConfMapping(String confMapping);
 }
